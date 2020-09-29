acae7f7d7f98a43751afd2798d2c773b204aba14
rubenrivastronc <rubencitorivas@gmail.com>
2020-09-29 11:03:24 +0200

    Prueba server nodejs

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const colors = require('colors');
+
+const handleServer = function(req,res){
+    res.writeHead(200, { 'Content-type': 'text/html'});
+    res.write('<h1>Hola mundo</h1>');
+    res.end();
+}
+
+const server = http.createServer(handleServer);
+
+server.listen(3000, function(){
+    console.log('Servidor en el puerto 3000'.yellow);
+});
